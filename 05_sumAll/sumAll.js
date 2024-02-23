bed5f701e0d4b54aa4bb1bd5904c81c2e417c30f
Kevin Vinchon <kevin.vinchon@gmail.com>
2024-02-23 15:57:01 +0100

    Complete #5 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if (min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number')
+    return 'ERROR';
 
+  if (min > max) {
+    let newMin = max;
+    max = min;
+    min = newMin;
+  }
+
+  let sum = 0;
+  while (min <= max) {
+    sum += min;
+    min++;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line

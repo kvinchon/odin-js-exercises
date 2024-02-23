16490b93693e69e3c5c051db0a1f207858066c70
Kevin Vinchon <kevin.vinchon@gmail.com>
2024-02-23 15:40:03 +0100

    Complete #4 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr = []) {
+  const args = Array.from(arguments);
+  let i = 0;
+  while (i < arr.length) {
+    if (args.includes(arr[i])) {
+      arr.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line

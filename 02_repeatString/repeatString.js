08cc8bb5b0581adfea5d9414b6613a4ed35e062f
Kevin Vinchon <kevin.vinchon@gmail.com>
2024-02-23 15:02:47 +0100

    Complete #2 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (str, repeatNumber) {
+  if (repeatNumber < 0) return 'ERROR';
 
+  let newStr = '';
+  for (let i = 0; i < repeatNumber; i++) {
+    newStr += str;
+  }
+  return newStr;
 };
 
 // Do not edit below this line

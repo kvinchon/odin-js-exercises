a2fc0f11ca98c9faa1cc78bba9ff6bd6e67c89a4
Kevin Vinchon <kevin.vinchon@gmail.com>
2024-02-27 14:02:07 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (member) {
+  if (member < 0) return 'OOPS';
 
+  let seq = [0, 1];
+  while (seq[member] === undefined) {
+    let digits = seq.slice(-2);
+    seq.push(digits.reduce((total, value) => total + value));
+  }
+
+  return seq[member];
 };
 
 // Do not edit below this line

c639d12f32c1bf412b36ec025a76f882b6b112d5
Kevin Vinchon <kevin.vinchon@gmail.com>
2024-02-27 13:22:18 +0100

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str = '') {
+  str = str.replace(/[ ,;.:/?!]/g, '').toLowerCase();
+  let reversed = str.split('').reverse().join('');
+  if (reversed === str) return true;
+  return false;
 };
 
 // Do not edit below this line
